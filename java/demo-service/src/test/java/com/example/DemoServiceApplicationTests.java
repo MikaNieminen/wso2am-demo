5f74e4bc5a2f7109e51e516fd454528a5213ee7b
Timo Tapanainen <timo.tapanainen@gmail.com>
2015-10-26 22:18:45 +0200

    added test

--- java/demo-service/src/test/java/com/example/DemoServiceApplicationTests.java
+++ java/demo-service/src/test/java/com/example/DemoServiceApplicationTests.java
@@ -1,18 +1,45 @@
 package com.example;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DemoServiceApplication.class)
 @WebAppConfiguration
 public class DemoServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
 
+    private MockMvc mockMvc;
+
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype());
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void getCustomerByPin() throws Exception {
+        mockMvc.perform(get("/customers/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$firstname", is("Timo")))
+                .andExpect(jsonPath("$lastname", is("Tapanainen")));
+    }
 }
